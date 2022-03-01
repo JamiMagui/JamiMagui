54b24537b0b4fa58ed7e8368ed360bdc6b70167d
Jamie Maguire <90646819+JamiMagui@users.noreply.github.com>
2022-02-28 19:53:32 -0500

    Added features to PW generator
    Added copy to clipboard action and alert

--- PasswordGenerator/index.js
+++ PasswordGenerator/index.js
@@ -1,10 +1,10 @@
-let char = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '?', '~']
+let char = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '*', '?', '~']
 
 let password1 = document.getElementById("passwords1")
 let password2 = document.getElementById("passwords2")
 let password3 = document.getElementById("passwords3")
 let password4 = document.getElementById("passwords4")
-
+isAlive = false
 
 function getRandomItem() {
     var item = char[Math.floor(Math.random()*char.length)];
@@ -53,26 +53,45 @@ function getRandomPassword() {
         getRandomItem2(char);
         getRandomItem3(char);
         getRandomItem4(char);
-}}
+}
+isAlive = true;
+}
 
-function clickToCopy() {
-    /* Get the text field */
-    var copyText = password1;
-  
-    /* Select the text field */
-    copyText.select("passwords1");
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-     /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
-  
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
-
-    console.log(copyText)
-  }
-
-
-
+function copyToClipboard1(elementId) {
+    if(isAlive === true) {var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById("passwords1").innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    alert("your new password has been copied to the clipboard")
+  }}
+function copyToClipboard2(elementId) {
+    if(isAlive === true) {var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById("passwords2").innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    alert("your new password has been copied to the clipboard")
+  }}
+function copyToClipboard3(elementId) {
+    if(isAlive === true) {var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById("passwords3").innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    alert("your new password has been copied to the clipboard")
+  }}
+function copyToClipboard4(elementId) {
+    if(isAlive === true) {var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById("passwords4").innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    alert("your new password has been copied to the clipboard")
+  }}
 
 
